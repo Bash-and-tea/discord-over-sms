84b4cca12ba4efa641904b2ced13d16b8c1f06a7
Bash-and-tea <33610863+Bash-and-tea@users.noreply.github.com>
2021-12-23 20:00:15 -0700

    Shell script; PostWebhook() added

--- forward.go
+++ forward.go
@@ -1,1 +1,32 @@
 package discord_over_sms
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func PostWebhook() int {
+	var bod io.Reader
+	bod = strings.NewReader("{\"username\": \"test\", \"content\": \"hello\"}")
+	resp, err := http.Post("https://discord.com/api/webhooks/921964797673742376/T4CW-_-BFANBKKTqcPc3vvmBqymKWZcvDFGWNZkL81JK-M7C4wtlOHsmBk7Koch8lJmb",
+		"application/json",
+		bod)
+	if err != nil {
+		log.Fatal(err)
+	}
+	body, err := io.ReadAll(resp.Body)
+	code := resp.StatusCode
+	resp.Body.Close()
+
+	if resp.StatusCode > 299 {
+		log.Fatalf("Response failed with status code: %d and\nbody: %s\n", resp.StatusCode, body)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s", body)
+	return code
+}
---
+++ testsend.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+curl \
+  -H "Content-Type: application/json" \
+  -d '{"username": "test", "content": "hello"}' \
+  'https://discord.com/api/webhooks/921964797673742376/T4CW-_-BFANBKKTqcPc3vvmBqymKWZcvDFGWNZkL81JK-M7C4wtlOHsmBk7Koch8lJmb'
